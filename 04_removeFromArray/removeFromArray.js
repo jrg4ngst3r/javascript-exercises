74c9c455c45cb2581590f852c7ca75eaed2f05bd
jrpp_199 <jrpp198@gmail.com>
2022-12-13 13:40:30 -0300

    made 1 to 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, value) {
+    let newArr = [];
+    let indexToRemove = [];
 
+    if (!Array.isArray(value)) {
+        arr.forEach(elemento => {
+            if (elemento != value) {
+                newArr.push(elemento);
+            }
+        });
+    } else {
+        arr.forEach(elemento => {
+            value.forEach((valor) => {
+                if (elemento != valor) {
+                    indexToRemove = indexToRemove.push(arr.indexOf(elemento));
+                }
+            });
+
+        });
+        indexToRemove = [...new Set(indexToRemove)];
+        for (let k = 0; k < arr.length; k++) {
+            for (let j = 0; j < value.length; j++) {
+                if (k != indexToRemove[j]) {
+                    newArr.push(arr[k]);
+                }
+            }
+        }
+    }
+
+    return newArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
